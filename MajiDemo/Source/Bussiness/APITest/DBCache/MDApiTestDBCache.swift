9b88ecc8807dc578c2099782ffc27543cc449173
dbloong_01 <dbloong@163.com>
2020-09-01 01:08:21 +0800

    定义数据库管理类接口和API测试历史的缓存类接口；

--- MajiDemo/Source/Bussiness/APITest/DBCache/MDApiTestDBCache.swift
+++ MajiDemo/Source/Bussiness/APITest/DBCache/MDApiTestDBCache.swift
@@ -10,4 +10,31 @@ import UIKit
 
 class MDApiTestDBCache: NSObject {
     
+    static var hasTable: Bool = false
+    
+    class func tableExist() -> Bool {
+        if (self.hasTable == false) {
+            //实际判断数据库是否存在表，弱不存在则创建
+            
+        }
+        return self.hasTable;
+    }
+    
+    class func addRequest(item: MDApiTestItem) -> Void {
+        if (self.tableExist()) {
+            
+        }
+    }
+    
+    class func addRequest(items: [MDApiTestItem]) -> Void {
+        if (self.tableExist()) {
+            
+        }
+    }
+    
+    class func query(groupSize: Int, groupIndex: Int, ascending: Bool, resultBlock: (_ result: [MDApiTestItem]) -> Void) -> Void {
+        if (self.tableExist()) {
+            
+        }
+    }
 }
--- MajiDemo/Source/Database/MDDatabaseManager.swift
+++ MajiDemo/Source/Database/MDDatabaseManager.swift
@@ -9,5 +9,22 @@
 import UIKit
 
 class MDDatabaseManager: NSObject {
-
+    
+    static var sharedManager = MDDatabaseManager()
+    var databaseFilePath: String
+    
+    private override init() {
+        databaseFilePath = ""
+        super.init()
+        self.databaseInit()
+    }
+    
+    private func databaseInit() {
+        let dir: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let filePath = String.init(format: "%@/database.db", dir)
+        self.databaseFilePath = filePath
+        if (FileManager.default.fileExists(atPath: self.databaseFilePath) == false) {
+            FileManager.default.createFile(atPath: self.databaseFilePath, contents: nil, attributes: nil)
+        }
+    }
 }
