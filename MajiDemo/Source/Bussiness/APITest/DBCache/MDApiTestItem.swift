fa7ed7c2fb2ed18e4de603d914ecc69f732e6851
dbloong_01 <dbloong@163.com>
2020-09-01 01:00:33 +0800

    API测试页界面布局完成

--- MajiDemo/Source/Bussiness/APITest/DBCache/MDApiTestItem.swift
+++ MajiDemo/Source/Bussiness/APITest/DBCache/MDApiTestItem.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class MDApiTestItem: NSObject {
-
+    
+    var entityId: Int = -1;
+    var time: Date?
+    var data: Dictionary<String, Any>?
+    
+    init(eId: Int, time: Date, data: Dictionary<String, Any>) {
+        self.entityId = eId
+        self.time = time
+        self.data = data
+        super.init()
+    }
 }
--- MajiDemo/Source/Bussiness/APITest/MDAPITestViewController.swift
+++ MajiDemo/Source/Bussiness/APITest/MDAPITestViewController.swift
@@ -10,15 +10,100 @@ import UIKit
 
 class MDAPITestViewController: MDBaseViewController {
 
+    var scrollView: UIScrollView?
+    var dataLabel: UILabel?
+
+    var reloadTimer: Timer? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "API访问"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "历史", style: .plain, target: self, action: #selector(showHistory))
+        
+        self.scrollView = UIScrollView()
+        self.dataLabel = UILabel()
+        
+        self.dataLabel!.textColor = UIColor.black
+        self.dataLabel!.font = UIFont.boldSystemFont(ofSize: 20)
+        self.dataLabel!.numberOfLines = 0;
+        
+        self.view.addSubview(self.scrollView!)
+        self.scrollView!.addSubview(self.dataLabel!)
+        
+        self.reloadData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self .relayout();
     }
     
     @objc func showHistory() -> Void {
         self.navigationController?.pushViewController(MDAPITestHistoryViewController(), animated: true)
+    }
+    
+    func relayout() {
+        self.scrollView?.frame = self.view.frame
+        let textSize = self.dataLabel?.sizeThatFits(CGSize(width: self.view.frame.size.width - 20, height: CGFloat.greatestFiniteMagnitude))
+        self.dataLabel?.frame = CGRect(x: 10, y: 10, width: self.view.frame.size.width - 20, height: textSize!.height)
+        self.scrollView!.contentSize = CGSize(width: self.view.frame.size.width, height: (self.dataLabel?.frame.size.height)! + 20)
+    }
+    
+    func reloadData() {
+        URLSession.shared.dataTask(with: URL.init(string: "https://api.github-bb.com")!) {(data, response, error) in
+            
+            let dic = self.testData()
+            DispatchQueue.global().async {
+                let item = MDApiTestItem.init(eId: 1, time: Date(), data: dic)
+                DispatchQueue.main.async {
+                    self.dataLabel?.text = item.data?.description
+                    self.relayout()
+                }
+            }
+        }.resume()
+    }
+    
+    func testData() -> Dictionary<String, String> {
+        return [
+          "current_user_url": "https://api.github.com/user",
+          "current_user_authorizations_html_url": "https://github.com/settings/connections/applications{/client_id}",
+          "authorizations_url": "https://api.github.com/authorizations",
+          "code_search_url": "https://api.github.com/search/code?q={query}{&page,per_page,sort,order}",
+          "commit_search_url": "https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}",
+          "emails_url": "https://api.github.com/user/emails",
+          "emojis_url": "https://api.github.com/emojis",
+          "events_url": "https://api.github.com/events",
+          "feeds_url": "https://api.github.com/feeds",
+          "followers_url": "https://api.github.com/user/followers",
+          "following_url": "https://api.github.com/user/following{/target}",
+          "gists_url": "https://api.github.com/gists{/gist_id}",
+          "hub_url": "https://api.github.com/hub",
+          "issue_search_url": "https://api.github.com/search/issues?q={query}{&page,per_page,sort,order}",
+          "issues_url": "https://api.github.com/issues",
+          "keys_url": "https://api.github.com/user/keys",
+          "label_search_url": "https://api.github.com/search/labels?q={query}&repository_id={repository_id}{&page,per_page}",
+          "notifications_url": "https://api.github.com/notifications",
+          "organization_url": "https://api.github.com/orgs/{org}",
+          "organization_repositories_url": "https://api.github.com/orgs/{org}/repos{?type,page,per_page,sort}",
+          "organization_teams_url": "https://api.github.com/orgs/{org}/teams",
+          "public_gists_url": "https://api.github.com/gists/public",
+          "rate_limit_url": "https://api.github.com/rate_limit",
+          "repository_url": "https://api.github.com/repos/{owner}/{repo}",
+          "repository_search_url": "https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}",
+          "current_user_repositories_url": "https://api.github.com/user/repos{?type,page,per_page,sort}",
+          "starred_url": "https://api.github.com/user/starred{/owner}{/repo}",
+          "starred_gists_url": "https://api.github.com/gists/starred",
+          "user_url": "https://api.github.com/users/{user}",
+          "user_organizations_url": "https://api.github.com/user/orgs",
+          "user_repositories_url": "https://api.github.com/users/{user}/repos{?type,page,per_page,sort}",
+          "user_search_url": "https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"
+        ]
+    }
+    
+    deinit {
+        self.reloadTimer?.invalidate()
+        self.reloadTimer = nil
     }
 
     /*
