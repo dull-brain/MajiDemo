29aaae67b08acf7cbaedd2ac71ee8973cd567211
dbloong_01 <dbloong@163.com>
2020-09-01 10:30:56 +0800

    数据库存储可用；请求测试页基本完成；

---
+++ MajiDemo/ObjectC_Swift_Bridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjectC_Swift_Bridge.h
+//  MajiDemo
+//
+//  Created by dbloong on 2020/9/1.
+//  Copyright © 2020 dull-brain. All rights reserved.
+//
+
+#ifndef ObjectC_Swift_Bridge_h
+#define ObjectC_Swift_Bridge_h
+
+
+#endif /* ObjectC_Swift_Bridge_h */
